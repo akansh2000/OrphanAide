3d5b329a03c5404304126f713d28cf900446785f
Prabhat <pksg2001@gmail.com>
2022-01-31 17:05:13 +0530

    Prabhat's Update

--- src/App.js
+++ src/App.js
@@ -1,39 +1,26 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { useEffect } from "react";
-import {
-  loginUser,
-  readUserData,
-  registerWithEmailAndPassword,
-  sendPasswordReset,
-  writeUserData,
-} from "./Firebase";
+//import { createUserWithEmailAndPassword } from "firebase/auth";
+//import { useEffect } from "react";
+// import {
+//   loginUser,
+//   readUserData,
+//   registerWithEmailAndPassword,
+//   sendPasswordReset,
+//   writeUserData,
+// } from "./Firebase";
+import Login from "./Login";
+import Signup from "./Signup";
 
 function App() {
-  useEffect(
-    () => registerWithEmailAndPassword("as369@snu.edu.in", "kuru!@#12"),
-    []
-  );
+  // useEffect(
+  //   () => registerWithEmailAndPassword("ps827@snu.edu.in", "kuru!@#12"),
+  //   []
+  // );
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Signup/>
+    );
 }
 
 export default App;
